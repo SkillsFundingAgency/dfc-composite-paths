4dc3695823ba2b023b815c87166ecb43aad890a2
Ilyas Dhin <ilyasdhin@gmail.com>
2019-11-20 12:59:32 +0000

    Fixed issues with delete. Renamed project to DFC.Composite.Paths.IntegrationTests

---
+++ DFC.Composite.Paths.IntegrationTests/PathServiceTests/GetAllTests.cs
@@ -0,0 +1,51 @@
+﻿using DFC.Composite.Paths.Common;
+using DFC.Composite.Paths.Services;
+using DFC.Composite.Paths.Storage;
+using DFC.Composite.Paths.Storage.Cosmos;
+using NUnit.Framework;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFC.Composite.Paths.IntegrationTests.PathServiceTests
+{
+    [TestFixture]
+    public class GetAllTests : PathServiceTestBase
+    {
+        private readonly string _collectionName = "getAll";
+
+        private IPathService _pathService;
+        private IDocumentStorage _documentStorage;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _documentStorage = new CosmosDocumentStorage(new CosmosConnectionString(CosmosConnectionString), CosmosPartitionKey, CosmosDatabase, _collectionName);
+            _pathService = new PathService(_documentStorage);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            var paths = await _pathService.GetAll();
+            foreach (var path in paths)
+            {
+                await _pathService.Delete(path.Path);
+            }
+        }
+
+        [Test]
+        public async Task Should_GetAll_WhenTheyExist()
+        {
+            var pathCount = 5;
+            for (var i = 1; i <= pathCount; i++)
+            {
+                var path = $"path{i}";
+                var newPath = Create(path, Layout.FullWidth);
+                await _pathService.Register(newPath);
+            }
+
+            var paths = await _pathService.GetAll();
+            Assert.AreEqual(pathCount, paths.Count());
+        }
+    }
+}
---
+++ DFC.Composite.Paths.IntegrationTests/PathServiceTests/GetTests.cs
@@ -0,0 +1,54 @@
+﻿using DFC.Composite.Paths.Common;
+using DFC.Composite.Paths.Services;
+using DFC.Composite.Paths.Storage;
+using DFC.Composite.Paths.Storage.Cosmos;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace DFC.Composite.Paths.IntegrationTests.PathServiceTests
+{
+    [TestFixture]
+    public class GetTests : PathServiceTestBase
+    {
+        private readonly string _collectionName = "get";
+        private readonly string _path = "path1";
+
+        private IPathService _pathService;
+        private IDocumentStorage _documentStorage;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _documentStorage = new CosmosDocumentStorage(new CosmosConnectionString(CosmosConnectionString), CosmosPartitionKey, CosmosDatabase, _collectionName);
+            _pathService = new PathService(_documentStorage);
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await _pathService.Delete(_path);
+        }
+
+        [Test]
+        public async Task Should_GetPath_ForExistingPath()
+        {
+            var newPath = Create(_path, Layout.FullWidth);
+
+            await _pathService.Register(newPath);
+            var existingPath = await _pathService.Get(newPath.Path);
+
+            Assert.IsNotNull(existingPath);
+            Assert.AreEqual(newPath.Path, existingPath.Path);
+            Assert.AreEqual(newPath.Layout, existingPath.Layout);
+        }
+
+        [Test]
+        public async Task Should_ReturnNull_ForNonExistingPath()
+        {
+            var existingPath = await _pathService.Get(Guid.NewGuid().ToString());
+
+            Assert.IsNull(existingPath);
+        }
+    }
+}
